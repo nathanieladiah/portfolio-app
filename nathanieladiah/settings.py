cf7642f3f6b67d0296ead21ff8264b3d78d783a4
nathanieladiah <nathanieladiah@gmail.com>
2021-12-15 12:35:04 -0400

    Add crispy forms to the templates

--- nathanieladiah/settings.py
+++ nathanieladiah/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
