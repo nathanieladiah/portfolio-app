8e7a13a533ec7e4ddcf49a8c78444e6d74a638a7
nathanieladiah <nathanieladiah@gmail.com>
2021-12-15 07:56:03 -0400

    Add database models
    Created the database models in models.py: 1 for posts and 1 for tags.
    
    Linked the models for access in the admin panel.
    Ran migrations and created a superuser account to access the admin.
    
    Created some posts in the admin panel and used those to load into the
    templates rather than the dictionary. Add tags to the template and added
    some css styling.
    
    Fix the links to link to specific posts and load that data from the
    database.

--- base/views.py
+++ base/views.py
@@ -1,32 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 
 def home(request):
-	return render(request, 'base/index.html')
+	posts = Post.objects.filter(active=True, featured=True)[0:3]
+
+	context = {'posts': posts}
+	return render(request, 'base/index.html', context)
 
 def posts(request):
+	# posts = Post.objects.all()
+	posts = Post.objects.filter(active=True)
 
-	posts = [
-		{
-			'headline': 'Headline # 1',
-			'sub_headline': 'Some random words to take up this space please.',
-		},
-		{
-			'headline': 'Headline # 2',
-			'sub_headline': 'More random words for the space taking up.',
-		},
-		{
-			'headline': 'Headline #3',
-			'sub_headline': 'Uggggggggggggggghhhhhhhhhhhhhhhhhhhh',
-		},
-	]
 	context = {'posts': posts}
 	return render(request, 'base/posts.html', context)
 
-def post(request):
-	return render(request, 'base/post.html')
+def post(request, pk):
+	post = Post.objects.get(id=pk)
+	context = {'post': post}
+	return render(request, 'base/post.html', context)
 
 def profile(request):
 	return render(request, 'base/profile.html')
