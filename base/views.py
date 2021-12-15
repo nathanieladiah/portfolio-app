e04228ef564a6fc3503c3476790b6f81444f3e78
nathanieladiah <nathanieladiah@gmail.com>
2021-12-15 07:17:47 -0400

    Install template
    Copied files from homepage static website and stored them in the correct
    static directories. Updated the home view to render index.html instead.
    
    Updated the links in index.html to load from static and image sources.
    Removed navbar section to navbar.html for use in all templates.
    
    Updated the routes in the javascript for the css so it would account for
    the static directory
    
    Update the posts page to load data programatically, and created some
    dummy test data in a dictionary to test the template.

--- base/views.py
+++ base/views.py
@@ -4,10 +4,26 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return render(request, 'base/home.html')
+	return render(request, 'base/index.html')
 
 def posts(request):
-	return render(request, 'base/posts.html')
+
+	posts = [
+		{
+			'headline': 'Headline # 1',
+			'sub_headline': 'Some random words to take up this space please.',
+		},
+		{
+			'headline': 'Headline # 2',
+			'sub_headline': 'More random words for the space taking up.',
+		},
+		{
+			'headline': 'Headline #3',
+			'sub_headline': 'Uggggggggggggggghhhhhhhhhhhhhhhhhhhh',
+		},
+	]
+	context = {'posts': posts}
+	return render(request, 'base/posts.html', context)
 
 def post(request):
 	return render(request, 'base/post.html')
